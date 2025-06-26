7cd8661e5358dcc4890401b29fc91bba1606fdac
jeremy zeta <jeremy_zeta@yahoo.com>
2025-06-26 23:49:03 +0900

    first commit

---
+++ CSharpSample/ClsDelegates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSample {
+    internal class ClsDelegates {
+        delegate void dg_Print(int val);
+
+        void Print_Normal(int val) {
+            Console.WriteLine(val);
+        }
+
+        void Print_Timesten(int val) {
+            Console.WriteLine(val * 10);
+        }
+
+        void Print_Divideten(int val) {
+            Console.WriteLine(val / 10);
+        }
+
+        void PrintHelper(dg_Print dgPrint, int val) {
+            dgPrint(val);
+        }
+
+        void Main() {
+            PrintHelper(Print_Normal, 100);
+            PrintHelper(Print_Timesten, 100);
+            PrintHelper(Print_Divideten, 100);
+        }
+    }
+}
