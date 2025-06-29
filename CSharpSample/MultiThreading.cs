69abf1b08fe130e187a97c885091b80321317e6c
jeremy zeta <jeremy_zeta@yahoo.com>
2025-06-29 20:37:01 +0900

    Simple Multi Threading

---
+++ CSharpSample/MultiThreading.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CSharpSample {
+    internal class MultiThreading {
+        void Main() {
+            for (int i = 0; i < 7; i++) {
+                Thread th = new Thread(delegate () { Run(i); });
+                th.Start();
+                th.Join();
+            }
+        }
+
+        void Run(int i) { 
+            
+        }
+    }
+
+    class AsyncTest {
+        void Main() {
+            Run();
+        }
+
+        async void Run() {
+            Task<int> tsk = new Task<int>(RetVal);
+            tsk.Start();
+
+            int res = await tsk;
+            Console.WriteLine(res);
+        }
+
+        int RetVal() {
+            return 1;
+        }
+    }
+}
